f5e87333256b3f5292ccae1b0fcecf813106b015
Manue <manuellaouedraogo275@gmail.com>
2026-02-26 17:33:13 +0000

    modification pom, ajout version lombok

--- src/main/java/com/smartmobilitytripmanagement/error/ErrorDetails.java
+++ src/main/java/com/smartmobilitytripmanagement/error/ErrorDetails.java
@@ -2,11 +2,13 @@ package com.smartmobilitytripmanagement.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
+@AllArgsConstructor // Génère le constructeur avec les 4 arguments
+@NoArgsConstructor
 public class ErrorDetails {
     private LocalDateTime timestamp;
     private String message;
