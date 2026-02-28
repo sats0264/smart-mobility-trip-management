01952b55d31eb10cf368900ed98e7e8f1fdf9591
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-28 13:11:55 +0000

    refactor application properties and Trip class; simplify getters/setters with Lombok

--- src/main/java/com/smartmobilitytripmanagement/beans/Trip.java
+++ src/main/java/com/smartmobilitytripmanagement/beans/Trip.java
@@ -1,9 +1,13 @@
 package com.smartmobilitytripmanagement.beans;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 import java.time.LocalDateTime;
+@Setter
+@Getter
 @Entity
 @Table(name = "trips")
 public class Trip {
@@ -41,62 +45,6 @@ public class Trip {
         this.startTime = LocalDateTime.now();
         this.status = "STARTED";
     }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getUserId() {
-        return userId;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-    public String getTransportType() {
-        return transportType;
-    }
-    public void setTransportType(String transportType) {
-        this.transportType = transportType;
-    }
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-    public String getStartLocation() {
-        return startLocation;
-    }
-    public void setStartLocation(String startLocation) {
-        this.startLocation = startLocation;
-    }
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
-    public String getEndLocation() {
-        return endLocation;
-    }
-    public void setEndLocation(String endLocation) {
-        this.endLocation = endLocation;
-    }
-    public String getStatus() {
-        return status;
-    }
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    public Double getCalculatedPrice() {
-        return calculatedPrice;
-    }
-    public void setCalculatedPrice(Double calculatedPrice) {
-        this.calculatedPrice = calculatedPrice;
-    }
-
 
 
 }
--- src/main/java/com/smartmobilitytripmanagement/service/TripService.java
+++ src/main/java/com/smartmobilitytripmanagement/service/TripService.java
@@ -1,12 +1,7 @@
 package com.smartmobilitytripmanagement.service;
 
 import com.smartmobilitytripmanagement.beans.Trip;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 
